b7211347741c17ad733e163978fda5ee194ff0e1
Mrglxor <muhamadfarhan.inc@gmail.com>
2025-05-04 17:33:29 +0700

    remove web server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,3 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on https://server-dracobase.verce.app:${PORT}`);
-});
+export default app;
